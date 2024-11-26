9e998512f1c2781547a5db2e90f0078c05d22214
Kaaviyan <kaaviyan@pixalive.me>
2024-11-26 16:15:41 +0530

    hlo

---
+++ rewardify/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Contents from './componenets/Dashboard/Contents/Contents';
+import Header from './componenets/Dashboard/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Contents/>
+    </div>
+  );
+}
+
+export default App;
